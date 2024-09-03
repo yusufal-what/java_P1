6511019ffd01eda68ba79df1ff4dc4f595300661
yusufal-what <yusufalkori43@gmail.com>
2024-09-03 20:24:13 +0700

    Add files via upload

---
+++ com/mycompany/_yusup_tugas2p/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._yusup_tugas2p;
+
+/**
+ *
+ * @author YUSUP PALKORI
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("NPM         : 2310631170058\nNAMA        : Yusup Palkori\nNilai Absen : 90\nNilai Tugas : 100\nNilai UTS   : 84\nNilai UAS   : 87\nNilai Akhir : 90"
+                + "");
+    }
+}
